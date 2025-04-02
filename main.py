5701a92faee16513bb6489925a5f5951a4c0b8bb
Marcos Miotto <marcos@miotto.dev>
2025-04-02 10:33:15 -0300

    Codigo comentado

--- main.py
+++ main.py
@@ -2,12 +2,6 @@ from sklearn.datasets import load_iris, load_wine
 from bisecting import BisectingKMeansAlgorithm
 from k_mens import ClusteringKmeans
 
-iris = load_iris()
-wine = load_wine()
-kmeansIris = ClusteringKmeans(iris)
-kmeansIris.plotDataset() #plotar um gráfico mostrando os atributos do meu dataset
-kmeansIris.calcutateWcss()
-kmeansIris.optimal_number_of_clusters()
-kmeansIris.plotKmeansQtdOtima() #USO DO PCA
-# iris_bisecting = BisectingKMeansAlgorithm("Iris", iris)
-# wine_bisecting = BisectingKMeansAlgorithm("Wine", wine)
+bisecting = BisectingKMeansAlgorithm("Wine", load_wine())
+bisecting.run_script()
+
